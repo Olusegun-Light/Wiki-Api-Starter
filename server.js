ca4649c6f582fe17827050c5c56a87a0e91d2625
Olusegun-Light <lightolusegun1@gmail.com>
2024-03-27 22:17:29 +0100

    [feat] :- added port to server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ mongoose.connect(DB).then(() => {
 });
 
 // Set up server
-const port = 7000;
+const port = process.env.PORT ||  7000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
